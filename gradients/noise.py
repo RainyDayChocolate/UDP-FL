a5048fdf3a0c311e981f0562d56cbedb8b96252b
Shuya Feng <>
2023-04-06 02:47:19 -0400

    changenoisescale

--- gradients/noise.py
+++ gradients/noise.py
@@ -1,5 +1,6 @@
 
 import torch
+import random
 
 ## Add Noise to Model gradients
 
@@ -34,9 +35,41 @@ class GaussianNoiseGenerator(NoiseGenerator):
     def mechanism(self, gradient: torch.Tensor):
         return torch.empty(gradient.size()).normal_(0, self.sensitivity / self.epsilon)
 
+class StaircaseNoiseGenerator(NoiseGenerator):
+    def __init__(self, sensitivity=0.1, epsilon=0.1, gamma=0.5):
+        self.sensitivity = sensitivity
+        self.epsilon = epsilon
+        self.gamma = gamma
+    def mechanism(self, gradient: torch.Tensor):
+        #device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+        # Convert parameters to tensors
+        epsilon = torch.tensor(self.epsilon)
+        gamma = torch.tensor(self.gamma)
+        sensitivity = torch.tensor(self.sensitivity)
+        # Get the shape of the gradients tensor
+        shape = gradient.shape
+
+        # Generate random values for sign, geometric_rv, unif_rv, and binary_rv
+        sign = torch.where(torch.rand(shape) < 0.5, -1, 1)
+        geometric_rv = torch.distributions.geometric.Geometric(1 - torch.exp(-epsilon)).sample(shape) - 1
+        unif_rv = torch.rand(shape)
+        binary_rv = torch.where(torch.rand(shape) < gamma / (gamma + (1 - gamma) * torch.exp(-epsilon)), 0, 1)
+
+        # Calculate noise based on the given mechanism
+        noise = sign * (
+            (1 - binary_rv) * ((geometric_rv + gamma * unif_rv) * sensitivity) +
+            binary_rv * ((geometric_rv + gamma + (1 - gamma) * unif_rv) * sensitivity)
+        )
+        
+        return noise
 
 class NoNoiseGenerator(NoiseGenerator):
+
+    # def __init__(self, sensitivity, epsilon, delta):
+    #     self.sensitivity = sensitivity
+    #     self.epsilon = epsilon
+    #     self.delta = delta
 
     def mechanism(self, gradient: torch.Tensor):
         return torch.zeros(gradient.size())
