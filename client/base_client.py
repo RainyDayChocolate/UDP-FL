60ba4d123b55680ec562041fd30ce6582fa708c2
kuangzheng <likuangzheng@hotmail.com>
2023-04-14 00:13:36 -0700

    Add noise to FedAvg

--- client/base_client.py
+++ client/base_client.py
@@ -14,7 +14,7 @@ class BaseClient:
                  model: BaseModel,
                  client_id,
                  importance=1,
-                 clipper=base_clip, 
+                 clipper=base_clip,
                  noise_generator=None):
         """_summary_
 
@@ -32,17 +32,16 @@ class BaseClient:
             self.noise_generator = NoNoiseGenerator()
         else:
             self.noise_generator = noise_generator
-    
+
         if clipper is None:
             self.clipper = base_clip
         else:
             self.clipper = clipper
-        # Default training method as train_for_model
         self.set_training_mode()
 
     def set_importance(self, importance):
         self.importance = importance
-    
+
     def set_fake_id(self, fakeid):
         self.fake_id = fakeid
 
@@ -54,11 +53,11 @@ class BaseClient:
         # What to upload
         self.model.eval()
         for param in self.model.parameters():
-            yield param.data + self.noise_generator.get_noise(param.data)
+            yield param.data
 
     @property
     def gradients(self):
-        # 
+        #
         self.model.train()
         # clip, add noise
         # clip(noise(param.grad.clone().detach()))
@@ -66,15 +65,14 @@ class BaseClient:
             clipped = self.clipper(grad)
             add_noise = lambda grad: grad + self.noise_generator.get_noise(grad)
             return add_noise(clipped)
-        
+
         # grads = [param.grad.clone().detach() for param in self.model.parameters]
         # clipped = [self.clipper(grad) for grad in grads]
         # noise_grad = [self.noise_generator.get_noise(grad) + grad for grad in cliped]
         # return noise_grad
-    
+
         return [helper(param.grad.clone().detach())
                 for param in self.model.parameters()]
-        return grad
 
     def set_training_mode(self, for_gradient=False):
         if for_gradient:
@@ -82,22 +80,39 @@ class BaseClient:
         else:
             self.train = self.train_for_model
 
+    def add_noise(self, params):
+        """This is a function to add noise to the params inplace, very dangerous
+
+        Args:
+            params: Pytorch parameter
+
+        """
+        for param in params:
+            grad = param.grad
+            clipped = self.clipper(grad)
+            param.grad = clipped + self.noise_generator.get_noise(clipped)
+
     def train_for_model(self, dataset, client_epochs=2):
         # Set the model to training mode
         self.model.train()
         # Get the loss sum
         for _ in range(client_epochs):
             for inputs, labels in dataset:
-                self.model.train_for_model(inputs, labels)
+                self.model.zero_grad()
+                self.model.get_gradient(inputs, labels)
+                self.add_noise(self.model.parameters())
+                self.model.step()
 
     def train_for_gradient(self, dataset, client_epochs=1):
         # Set the model to evaluation mode
         self.model.train()
         for _ in range(client_epochs):
             for inputs, labels in dataset:
-                self.model.train_for_gradient(inputs, labels)
+                self.model.zero_grad()
+                self.model.get_gradient(inputs, labels)
+                # Should add yield gradient here
+                self.model.step()
 
     @property
     def fake_device_id(self):
         return random.random()
-    
--- models/base_model.py
+++ models/base_model.py
@@ -21,18 +21,17 @@ class BaseModel:
         for param in self.parameters():
             param.data = torch.zeros_like(param.data)
 
-    def train_for_gradient(self, inputs, labels):
-        self.optimizer.zero_grad()
-        output = self.forward(inputs)
-        loss = self.loss_fn(output, labels)
-        loss.backward()
+    def get_gradient(self, inputs, labels):
+        """For this function, it should always work as follows
+        zero_grad() -> get_gradient() -> step() -> zero_grad() -> ...
 
-    def train_for_model(self, inputs, labels):
-        self.optimizer.zero_grad()
+        Args:
+            inputs (_type_): _description_
+            labels (_type_): _description_
+        """
         output = self.forward(inputs)
         loss = self.loss_fn(output, labels)
         loss.backward()
-        self.optimizer.step()
 
     def copy_a_new(self):
         return self.__class__()
